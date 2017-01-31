50ec4c221dbc5bc1ae9515fa9b432268f2b00f84
MACasuba <Macasuba@gmail.com>
2017-01-31 22:14:11 +0100

    Add files via upload

---
+++ aerial-tv/xcode/Aerial/AppDelegate.swift
@@ -0,0 +1,105 @@
+/*
+    From Apple's TVMLAudioVideo example app.
+*/
+
+import UIKit
+import TVMLKit
+import AVFoundation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    // MARK: Properties
+    
+    var window: UIWindow?
+    var appController: TVApplicationController?
+    
+    static let TVBaseURL = "http://n-hout.nl/data/storage/attachments/client/"
+    //static let TVBaseURL = "http://okofish.github.io/aerial/client/"
+    //static let TVBaseURL = "https://jesse.ws/tvos/aerial/"
+    
+    //static let TVBootURL = "\(TVBaseURL)js/application.js"
+    static let TVBootURL = "\(TVBaseURL)client.js"
+
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+    // Create the TVApplicationControllerContext for this application
+        let appControllerContext = TVApplicationControllerContext()
+    
+    // The JavaScript URL is used to create the JavaScript context for your TVMLKit application
+        if let javaScriptURL = URL(string: AppDelegate.TVBootURL) {
+            appControllerContext.javaScriptApplicationURL = javaScriptURL
+        }
+    
+    // Set the properties that will be passed to the `App.onLaunch` function in JavaScript
+        if let launchOptions = launchOptions {
+            for (kind, value) in launchOptions {
+                appControllerContext.launchOptions[kind.rawValue] = value as AnyObject
+            }
+        }
+        
+        
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        
+        return true
+    }
+ 
+ 
+    
+    // MARK: TVApplicationControllerDelegate
+    
+    func appController(_ appController: TVApplicationController, didFinishLaunching options: [String: Any]?) {
+        print("\(#function) invoked with options: \(options)")
+    }
+    
+    func appController(_ appController: TVApplicationController, didFail error: Error) {
+        print("\(#function) invoked with error: \(error)")
+        
+        let title = "Error Launching Application"
+        //error message
+        let message = error.localizedDescription
+        let alertController = UIAlertController(title: title, message: message, preferredStyle:.alert )
+        
+        self.appController?.navigationController.present(alertController, animated: true, completion: { () -> Void in
+            // ...
+        })
+    }
+    
+    func appController(_ appController: TVApplicationController, didStop options: [String: Any]?) {
+        print("\(#function) invoked with options: \(options)")
+    }
+ 
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+ 
+    
+
+
+    
+    
+    
+}
---
+++ aerial-tv/xcode/Aerial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Aerial
+//
+//  Created by MACasuba on 29-01-17.
+//  Copyright © 2017 appletv. All rights reserved.
+//
+
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}//end
+
+
+
